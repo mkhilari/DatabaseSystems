40d94e0fe6d74988e7da7f7b39bbdbfafc2260ea
Manish Khilari <mkhilari@Manishs-MacBook-Air.local>
2021-07-18 22:19:58 +1000

    hello added

--- hello.sql
+++ hello.sql
@@ -1,30 +1,7 @@
-CREATE TABLE Monke (
-    MonkeID INT NOT NULL, 
-    MonkeName VARCHAR(100), 
-    MonkeDOB DATE, 
-    MonkeColour ENUM('Black', 'Brown', "Grey"), 
-    OwnerID INT, 
-    ZooID INT,
-    PRIMARY KEY (MonkeID), 
-    FOREIGN KEY (OwnerID) REFERENCES Human(HumanID), 
-    FOREIGN KEY (ZooID) REFERENCES Zoo(ZooID)
-)
-
-CREATE TABLE Human (
-    HumanID INT NOT NULL, 
-    HumaName VARCHAR(100), 
-    HumanDOB DATE, 
-    MotherID INT, 
-    FatherID INT, 
-    PRIMARY KEY (HumanID), 
-    FOREIGN KEY (MotherID) REFERENCES Human(HumanID), 
-    FOREIGN KEY (FatherID) REFERENCES Human(HumanID)
-)
-
-CREATE TABLE Zoo (
-    ZooID INT NOT NULL, 
-    ZooName VARCHAR(100), 
-    ZooStartDate DATE, 
-    ZooCity VARCHAR(100), 
-    PRIMARY KEY (ZooID)
-)
+SELECT Da.Quarter, SUM(TP.UnitCost) AS sumOfUnitCost 
+FROM TreatmentPlan AS TP 
+INNER JOIN Date AS Da ON TP.DateID = Da.DateID 
+INNER JOIN Drug AS Dr ON TP.DrugID = Dr.DrugID
+WHERE Dr.Duration > 8
+GROUP BY Da.Quarter 
+;
